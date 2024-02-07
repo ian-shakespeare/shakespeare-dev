e2043ab94508b4c54e4724c9afeee286e6acb4c7
Ian Shakespeare <irshakes11@gmail.com>
2024-02-06 17:52:11 -0700

    wedding stubs

---
+++ src/routes/(wedding)/wedding/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+  <meta name="robots" content="noindex">
+</svelte:head>
+<main>
+  <section>
+    <h1>Seina &amp; Ian</h1>
+    <div class="flex">
+      <h2>March 16, 2024, 16:00</h2>
+      <h3>Nagoya</h3>
+    </div>
+  </section>
+</main>
